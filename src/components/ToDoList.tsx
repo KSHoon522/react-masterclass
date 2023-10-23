5783d18f492c14bda29d776fcd063eb092085bf3
KSH <KSHoon522@gamil.com>
2023-10-23 16:31:10 +0900

    Finish To Do List

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,87 +1,27 @@
-import { useForm } from "react-hook-form";
-
-interface IForm {
-  email: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  password1: string;
-  extraErrors?: string;
-}
+import { useRecoilState, useRecoilValue } from "recoil";
+import CreateToDo from "./CreateToDo";
+import { Categories, ToDoSelector, categoryState } from "../atoms";
+import ToDo from "./ToDo";
 
 function ToDoList() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<IForm>({ defaultValues: { email: "@naver.com" } });
-  const onValid = (data: IForm) => {
-    if (data.password !== data.password1) {
-      setError(
-        "password1",
-        { message: "Password are not the same" },
-        { shouldFocus: true }
-      );
-    }
-    setError("extraErrors", { message: "Server Offline" });
+  const toDos = useRecoilValue(ToDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value as any);
   };
-  console.log(errors);
   return (
     <div>
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={handleSubmit(onValid)}
-      >
-        <input
-          {...register("email", {
-            required: "email is required",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "only naver.com email allowed",
-            },
-          })}
-          placeholder="Email"
-        />
-        <span>{errors?.email?.message as string}</span>
-        <input
-          {...register("firstName", {
-            required: "Write here",
-            validate: {
-              noSang: (value) =>
-                value.includes("sang") ? "sang is not allwoed" : true,
-              noKim: (value) =>
-                value.includes("Kim") ? "Kim is not allwoed" : true,
-            },
-          })}
-          placeholder="First Name"
-        />
-        <span>{errors?.firstName?.message as string}</span>
-        <input
-          {...register("lastName", { required: "Write here" })}
-          placeholder="Last Name"
-        />
-        <span>{errors?.lastName?.message as string}</span>
-        <input
-          {...register("username", { required: "Write here" })}
-          placeholder="Username"
-        />
-        <span>{errors?.username?.message as string}</span>
-        <input
-          {...register("password", { required: "Write here" })}
-          placeholder="Password"
-        />
-        <span>{errors?.password?.message as string}</span>
-        <input
-          {...register("password1", { required: "Write here" })}
-          placeholder="Password1"
-        />
-        <span>{errors?.password1?.message as string}</span>
-
-        <button>Add</button>
-        <span>{errors?.extraErrors?.message}</span>
-      </form>
+      <h1>To Dos</h1>
+      <hr />
+      <select value={category} onInput={onInput}>
+        <option value={Categories.TO_DO}>To Do</option>
+        <option value={Categories.DOING}>Doing</option>
+        <option value={Categories.DONE}>Done</option>
+      </select>
+      <CreateToDo />
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
     </div>
   );
 }
